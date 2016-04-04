c2d163d4ee79e94c2171b6b21ab8988f3a9195c9
Rousseau Thibaut <thibaut.rousseau@soprasteria.com>
2016-04-04 10:20:00 +0200

    feat: Add model for sites

---
+++ sites/sites.model.go
@@ -0,0 +1,15 @@
+package sites
+
+import (
+	"gopkg.in/mgo.v2/bson"
+	"time"
+)
+
+// Site model
+type Site struct {
+	ID        bson.ObjectId `bson:"_id,omitempty"`
+	Created   time.Time     `bson:"created"`
+	Title     string        `bson:"title"`
+	Latitude  float64       `bson:"latitude"`
+	Longitude float64       `bson:"longitude"`
+}
