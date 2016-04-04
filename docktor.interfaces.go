b34c8713ce54250d3351fdd9644c9e8a06348635
Rousseau Thibaut <thibaut.rousseau@soprasteria.com>
2016-04-04 10:50:35 +0200

    feat: Add a Sites entrypoint to the API

--- docktor.interfaces.go
+++ docktor.interfaces.go
@@ -4,8 +4,9 @@ import (
 	"github.com/soprasteria/godocktor-api/daemons"
 	"github.com/soprasteria/godocktor-api/groups"
 	"github.com/soprasteria/godocktor-api/services"
-	"gopkg.in/mgo.v2"
+	"github.com/soprasteria/godocktor-api/sites"
 	"github.com/soprasteria/godocktor-api/users"
+	"gopkg.in/mgo.v2"
 )
 
 // Session is the interface for a docktor sessio
@@ -20,5 +21,6 @@ type Client interface {
 	Groups() groups.RepoGroups
 	Daemons() daemons.RepoDaemons
 	Users() users.RepoUsers
+	Sites() sites.RepoSites
 	Close()
 }
